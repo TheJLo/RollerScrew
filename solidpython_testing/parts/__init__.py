2954b3991fb77ac9457106900bfde8b53cd32b23
TheJLo <jmloss@mtu.edu>
2020-01-18 02:58:39 -0500

    Forgot to delete old stuff

--- solidpython_testing/parts/__init__.py
+++
@@ -1,6 +0,0 @@
-# I don't really know what I am doing...
-
-from .rsgl_part import RSGL_Part
-from .rsgl_1_nut import RSGL_Nut
-from .rsgl_2_roller import RSGL_Roller
-from .rsgl_3_screw import RSGL_Screw
