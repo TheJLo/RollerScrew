dba3a56365f6c94f9140a5ad49282a624ec07ac9
Jacob Loss <jmloss@mtu.edu>
2020-02-04 13:07:20 -0500

    File restructure for merging

--- parts/rsgl_3_screw/rsgl_3_screw.scad
+++
@@ -1,7 +0,0 @@
-// Center Screw Module
-
-module center_screw(ref_dia = 15, height = 50, lead = 10, starts = 5){
-    
-    cylinder(d = ref_dia, h = height, center = true);
-    
-}
---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+# Setup file for Roller Screws
+# Use this file to build the virtual envrioment
+# and to create the install file which tells the 
+# main program where to put everything
+
+# First create virtual envrioment
+
+# Install necessary packages from requirements.txt
+
+# create install.config
+
